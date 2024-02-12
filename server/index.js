4002c33666f53895147b7a04588e5e91c34a702b
arifshaikh80 <arifshaikh8072@gmail.com>
2024-02-12 14:48:53 +0530

    errorfix

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
+app.use('/.netlify/server/index',router);
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'));
